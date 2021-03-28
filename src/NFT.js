1b0f26dfa592c830c37918246fa168122ac24ce2
mahnoorshahid <mahnoorshahid09@gmail.com>
2021-03-28 18:42:43 -0400

    results

--- src/NFT.js
+++ src/NFT.js
@@ -25,20 +25,38 @@ const data = [{
         creator: 'creator',
         hasOwner: false,
         owner: 'owner name'
+    },
+    {
+        nftName: '2 Psyche',
+        eth: "0.2",
+        description: (
+            <>
+            <p>
+            83m² plot title parcel on Helios, 778m from the origin, with a 8m build height, floor is at 0m elevation
+            </p>
+            </>
+        ),
+        date: "March 3rd",
+        creator: 'creator',
+        hasOwner: false,
+        owner: 'owner name'
     }
 ]
 
-const NFT = (nft) => {
+const NFT = () => {
         const id = useParams();
-       const {collection, price} = nft;
         const [assets, setAssets] = useState([]);
-        const [title, setTitle] = useState('default');
+        const [title, setTitle] = useState('default title');
 
         const getAssets = async() => {
          const response = await fetch(url);
          const assets = await response.json();
-          console.log("bundle nft", assets);
+
+         const result = assets.assets[0];
+          console.log("assets", assets);
+          console.log("result", result);
           setAssets(assets);
+          setTitle(result.id);
         };
 
         // useEffect(() => {
@@ -49,20 +67,8 @@ const NFT = (nft) => {
 
         useEffect(() =>{
             getAssets();
-         //   const base = assets && assets.bundles;
-        // const data = base && base.map((item) => [item.assets]);
-           // const result = data && data.map((item) => item.find((nftItem) => nftItem.token_id === parseInt(id)));
-        //  const assetBundles = assets && assets.bundles && assets.bundles.length >= 0 && assets.bundles.map((item) => [item.assets]);
 
-         //  const assetBundles = base && base.map((item) => item);
-            // const result = assets && assets.map((item)=>item.find((nftItem) => nftItem.id === parseInt(id)));
-           //const selectedNFT = assets && assets.find((nftItem) => nftItem.token_id === parseInt(id));
-
-           //return boolean
-           //const selectedNFT = assets && assets.length > 0 && assets.map((nftItem) => nftItem.id === parseInt(id));
-
-           const selectedNFT = assets && assets.length > 0 && assets.find((nftItem) => nftItem.id === parseInt(id));
-
+           const selectedNFT = assets && assets.assets && assets.assets.length > 0 && assets.assets.find((nft) => nft.id === parseInt(id));
 
             setTitle(selectedNFT);
             //returning false
@@ -70,24 +76,39 @@ const NFT = (nft) => {
 
         }, []);
         console.log("PARAMS",useParams(assets));
-        console.log("props", nft);
       
 
         return (
             <>
-        <div className="eventContainer">
+        <div className="maxWidth">
         
-            <h1>collection.name</h1>
+            <h1>collection.name {assets.id}</h1>
             <h1>
-                {collection}
+             {title}
             </h1>
             {/* <h2>{props.id}</h2> */}
-
-         {assets && assets.bundles && assets.bundles.length > 0 && assets.map((item)=>{
+            {assets  && assets.length > 0 && assets.map((item)=>{
+                console.log("item", item);
+                {item && item.assets.length > 0 && item.assets.map((nft) =>{
+                    console.log("nft", nft);
+                        const { image_preview_url, collection, token_id, id} = nft;
+                    
+                    return <div>
+                        <h1>
+                            {nft.id}
+                        </h1>
+                        </div>
+                })};
+     
+                 })}
+         {/* {assets && assets.bundles && assets.bundles.length > 0 && assets.map((item)=>{ */}
+            {assets && assets.length >= 0 && assets.map((item)=>{
+                console.log('item',item);
                 {/* console.log("in nft", item); */}
                 return (
-            
+                    
                 <li  className="flexLeft" key={item.id}>
+                {/* <p>{collection.name}</p> */}
                     {item && item.assets.length > 0 && item.assets.map((nft) =>{
                         {/* console.log("nft2", nft); */}
                         const { image_preview_url, collection, token_id, description} = nft;
